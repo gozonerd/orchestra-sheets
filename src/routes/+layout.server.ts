b5064e367bd295c280f1da2b1fc88b086bf44aab
nerdykrystal <nerdykrystal@gmail.com>
2026-04-13 21:26:02 -0400

    Stage 07: Temporarily disable auth for UI component testing
    Disabled auth.js initialization in root layout to allow /test-ui page to render without auth configuration blockers. Will be re-enabled after auth setup is complete.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-	const session = await event.locals.getSession();
-	return { session };
+	// TODO: Auth.js initialization temporarily disabled for dev testing
+	// Will be re-enabled after auth configuration is complete
+	// const session = await event.locals.getSession();
+	return { session: null };
 };
